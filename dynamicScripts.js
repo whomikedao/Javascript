415801b9963bca0a6df292ee469bb94429874fb6
Michael Dao <michaeltuandao@gmail.com>
2019-05-08 09:33:27 -0500

    basis edit for dynamic scripting

--- dynamicScripts.js
+++ dynamicScripts.js
@@ -5,33 +5,37 @@ var header = document.createElement('h1');
 header.textContent = 'HighOnCoding';
 
 mainDiv.appendChild(header); 
-header.setAttribute('class', 'navbar-brand bg-primary text-light')
+mainDiv.setAttribute('class', 'container-fluid')
+header.setAttribute('class', 'h-100 p-4 bg-primary text-light')
 
 //Creating first content box "Curse of the Current Reviews"
 var div1 = document.createElement('div'); 
 var div1Header = document.createElement('h2')
 div1Header.textContent = "Curse of the Current Reviews";
 var divInnerOne = document.createElement('div');
-divInnerOne.textContent = "content content content";
+divInnerOne.textContent = "The giant squid is a deep-ocean dwelling squid in the family Architeuthidae. Giant squid can grow to a tremendous size due to deep-sea gigantism: recent estimates put the maximum size at 13 m for females and 10 m for males from the posterior fins to the tip of the two long tentacles. The giant squid is a deep-ocean dwelling squid in the family Architeuthidae. Giant squid can grow to a tremendous size due to deep-sea gigantism: recent estimates put the maximum size at 13 m for females and 10 m for males from the posterior fins to the tip of the two long tentacles.";
 
 mainDiv.appendChild(div1);
 div1.appendChild(div1Header);
 div1.appendChild(divInnerOne);
-div1.setAttribute('class', 'bg-primary text-light')
+div1.setAttribute('class', 'm-5 p-4 bg-secondary text-light')
 
 //Creating second content box "Hello WatchKit"
 var div2 = document.createElement('div');
 var div2Header = document.createElement('h3');
 div2Header.textContent = "Hello WatchKit";
 var div2InnerOne = document.createElement('div');
-div2InnerOne.textContent = "Last month...";
+div2InnerOne.textContent = "The giant squid is a deep-ocean dwelling squid in the family Architeuthidae. Giant squid can grow to a tremendous size due to deep-sea gigantism: recent estimates put the maximum size at 13 m for females and 10 m for males from the posterior fins to the tip of the two long tentacles.The giant squid is a deep-ocean dwelling squid in the family Architeuthidae. Giant squid can grow to a tremendous size due to deep-sea gigantism: recent estimates put the maximum size at 13 m for females and 10 m for males from the posterior fins to the tip of the two long tentacles.";
 var div2Comments = document.createElement('div');
-div2Comments.textContent = "12 Comments";
+div2Comments.textContent = "12 Comments 14 Likes";
 
 mainDiv.appendChild(div2);
 div2.appendChild(div2Header);
 div2.appendChild(div2InnerOne);
 div2.appendChild(div2Comments);
+div2.setAttribute('class', 'm-5');
+div2Header.setAttribute('class', 'text-primary')
+div2Comments.setAttribute('class', 'p-1 bg-warning text-light text-emphasis')
 
 
 //Creating third content box "Introduction to Swift"
@@ -39,7 +43,7 @@ var div3 = document.createElement('div');
 var div3Header = document.createElement('h3');
 div3Header.textContent = "Introduction to Swift";
 var div3InnerOne = document.createElement('div');
-div3InnerOne.textContent = "Swift is a modern...";
+div3InnerOne.textContent = "The giant squid is a deep-ocean dwelling squid in the family Architeuthidae. Giant squid can grow to a tremendous size due to deep-sea gigantism: recent estimates put the maximum size at 13 m for females and 10 m for males from the posterior fins to the tip of the two long tentacles.The giant squid is a deep-ocean dwelling squid in the family Architeuthidae. Giant squid can grow to a tremendous size due to deep-sea gigantism: recent estimates put the maximum size at 13 m for females and 10 m for males from the posterior fins to the tip of the two long tentacles.";
 var div3Comments = document.createElement('div');
 div3Comments.textContent = "15 comments";
 
@@ -47,6 +51,9 @@ mainDiv.appendChild(div3);
 div3.appendChild(div3Header);
 div3.appendChild(div3InnerOne);
 div3.appendChild(div3Comments);
+div3.setAttribute('class', 'm-5')
+div3Header.setAttribute('class', 'text-primary')
+div3Comments.setAttribute('class', 'p-1 bg-warning text-light')
 
 // function liTag(className, innerHTML, idName){
 //     var li = document.createElement('li');
